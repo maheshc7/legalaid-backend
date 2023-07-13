ac0da3f74abb5deddccc990a66da4b2acd9578bd
Mahesh <mrak4123@gmail.com>
2023-07-13 10:46:40 -0700

    import fix & beautify

--- controller.py
+++ controller.py
@@ -4,43 +4,63 @@ Handles the calls to make the Outlook API authentication
 Adds new events to the calendar.
 """
 
-from flask import Flask, request, jsonify
 import uuid
 import os
 import tempfile
-from pdfparser import PdfParser
 import concurrent.futures
+from flask import Flask, request, jsonify
+from pdfparser import PdfParser
 
 app = Flask(__name__)
+
 
 # Define a custom middleware function to handle CORS headers
 @app.after_request
 def add_cors_headers(response):
+    """
+    Adding CORS header
+    """
     # Replace '*' with the appropriate origin(s) allowed to make requests
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
+    """
+    Index page for the server
+    """
     return "You have reached the Homepage of LegalAid Backend"
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # Check if a file was included in the request
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file provided'}), 400
 
-    file = request.files['file']
-    
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    """
+    Checks if the request consists of a pdf file and 
+    saves it in as a temporary file before processing it.
+    Returns
+        200 : Suceess
+        500 : Error
+
+    """
+    # Check if a file was included in the request
+    if "file" not in request.files:
+        return jsonify({"error": "No file provided"}), 400
+
+    file = request.files["file"]
+
     # Check if the file has a PDF extension
-    if not file.filename.lower().endswith('.pdf'):
-        return jsonify({'error': 'Invalid file format, only PDF files are allowed'}), 400
+    if not file.filename.lower().endswith(".pdf"):
+        return (
+            jsonify({"error": "Invalid file format, only PDF files are allowed"}),
+            400,
+        )
 
     try:
         # Save the uploaded file to a temporary file
-        with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as temp_file:
+        with tempfile.NamedTemporaryFile(suffix=".pdf", delete=False) as temp_file:
             file.save(temp_file.name)
 
         # Process the PDF file in a separate thread
@@ -54,10 +74,14 @@ def upload_file():
         return jsonify(case_info), 200
 
     except Exception as error:
-        return jsonify({'error': str(error)}), 500
+        return jsonify({"error": str(error)}), 500
+
 
 def extract_details(file):
-    details ={}
+    """
+    Sends the file to pdfparser and returns the case and events in a json format (as dict).
+    """
+    details = {}
     parser = PdfParser(file)
     case_details = parser.get_case_details()
 
@@ -68,22 +92,23 @@ def extract_details(file):
         if event == "no event":
             continue
         event = event.title()
-        for task,date in subevent.items():
+        for task, date in subevent.items():
             task = task.capitalize()
             data = {
-                "id" : uuid.uuid4(),
+                "id": uuid.uuid4(),
                 "subject": event,
-                "date" : str(date.date()),
-                "description": task
+                "date": str(date.date()),
+                "description": task,
             }
 
             event_details.append(data)
-    
+
     details["case"] = case_details
     details["events"] = event_details
     details["length"] = len(event_details)
-    
+
     return details
 
-if __name__ == '__main__':
-    app.run( threaded=True)
+
+if __name__ == "__main__":
+    app.run(threaded=True)
--- pdfparser.py
+++ pdfparser.py
@@ -2,18 +2,11 @@
 Module for parsing the pdf and returning the events and their associated sub-events and dates
 """
 import re
-from io import StringIO
-import PyPDF2
 import spacy
 from dateparser.search import search_dates
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
 
 import fitz
+
 
 class PdfParser:
     """
@@ -88,29 +81,29 @@ class PdfParser:
         page = self.file.load_page(0)
         # extracting text from page
         content = page.get_text().lower()
-        caseNum = (
+        case_num = (
             re.search("(?:case no\.|case|no\.):?\s?([a-z]\w{5,})", content)
             .group(1)
             .replace(" ", "")
             .upper()
         )
         case_info = {
-            "caseNum": caseNum,
-            "court" : court,
+            "caseNum": case_num,
+            "court": court,
             "plaintiff": plaintiff,
-            "defendant": defendant
+            "defendant": defendant,
         }
         return case_info
-    
-    def extract_task(self,sentence):
+
+    def extract_task(self, sentence):
         """
-        This algorithm uses the spaCy library to tokenize and parse the input sentence. 
+        This algorithm uses the spaCy library to tokenize and parse the input sentence.
         First identifies the action verb in the sentence by finding the first verb in the sentence.
-        Then it looks for the object or complement of the verb by searching for related dependencies 
+        Then it looks for the object or complement of the verb by searching for related dependencies
         such as direct object, clausal complement, open clausal complement, or subject complement.
-        Checks if the current token has any left or right children in the dependency tree, 
-        which indicates that it is part of a noun chunk. 
-        If so, it includes all the tokens in the subtree of the current token as part of the task. 
+        Checks if the current token has any left or right children in the dependency tree,
+        which indicates that it is part of a noun chunk.
+        If so, it includes all the tokens in the subtree of the current token as part of the task.
         Otherwise, it just includes the current token as part of the task.
 
         If the extracted task is just a single word or has more than 15 words we return the sentence itself.
@@ -119,7 +112,7 @@ class PdfParser:
             sentence (string): A string
 
         Returns:
-            task (string): A string 
+            task (string): A string
         """
         doc = self.nlp(sentence)
         task = ""
@@ -129,7 +122,7 @@ class PdfParser:
                 break
         for token in doc:
             if token.lemma_ in ["plaintiff", "defendant", "attorney"]:
-                task = token.text +": "+task
+                task = token.text + ": " + task
 
             if token.text == task:
                 continue
@@ -140,9 +133,12 @@ class PdfParser:
                 else:
                     task += " " + token.text
         task = task.strip()
-        return (sentence if not (3<=len(task.split(" "))<=15) else task)
+        return sentence if not (3 <= len(task.split(" ")) <= 15) else task
 
     def extract_date(self, text):
+        """
+        Extract the dates using spacy library
+        """
         # process the text with spaCy
         doc = self.nlp(text)
         dates = []
@@ -151,7 +147,10 @@ class PdfParser:
             # check if the entity is a date
             if ent.label_ == "DATE":
                 # print the text and label of the date entity
-                date = search_dates(ent.text,settings={"STRICT_PARSING": False, "PARSERS": ["absolute-time"]},)
+                date = search_dates(
+                    ent.text,
+                    settings={"STRICT_PARSING": False, "PARSERS": ["absolute-time"]},
+                )
                 if date:
                     dates.append(date[0])
         return dates
@@ -165,16 +164,15 @@ class PdfParser:
         events = {}
         event = ""
         content = self.clean_page(self.content)
-        
         events["no event"] = {}
-        paragraphs = re.split("\n",content)
+        paragraphs = re.split("\n", content)
         for para in paragraphs:
             para = self.clean_page(para)
             new_events = re.findall("(\d{1,3}\.[A-Za-z()\-\, ]+)(?:\.|\:)", para)
             if new_events:
-                para = re.sub(r"(\d{1,3}\.[A-Za-z()\-\, ]+)(?:\.|\:)", r"\1:",para)
+                para = re.sub(r"(\d{1,3}\.[A-Za-z()\-\, ]+)(?:\.|\:)", r"\1:", para)
             new_event = re.search(r"\d{1,3}\. *([A-Za-z()\-\, ]{10,})(?:\:|\.)", para)
-            para = re.sub(r"\d{1,3}\. *([A-Za-z()\-\, ]{10,})(?:\:|\.)", "",para)
+            para = re.sub(r"\d{1,3}\. *([A-Za-z()\-\, ]{10,})(?:\:|\.)", "", para)
             if new_event:
                 event = new_event.group(1)
                 events[event] = {}
@@ -190,17 +188,17 @@ class PdfParser:
                 if not re_dates:
                     re_dates = []
 
-                dates = nlp_dates if (len(nlp_dates)>len(re_dates)) else re_dates
+                dates = nlp_dates if (len(nlp_dates) > len(re_dates)) else re_dates
 
                 if not event:
-                    event = 'no event'
+                    event = "no event"
 
-                if event and len(dates)>0:
+                if event and len(dates) > 0:
                     for date in dates:
                         lines = [line]
                         if len(dates) > 1:
                             lines = line.split(date[0])
-                        new_line =lines[0].replace(date[0],'')
+                        new_line = lines[0].replace(date[0], "")
                         task = self.extract_task(new_line)
                         if task in events[event]:
                             task = line
